b76e7c71cf4a2fd7fc34c97d4cbb51fa8e181203
muradav <muradav@code.edu.az>
2022-03-29 16:14:52 +0400

    feat:dersde verilen taskin tekraren ishlenilmesi

---
+++ Static.Example/Static.Example/Helper/Extention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static.Example.Helper
+{
+    static class Extention
+    {
+       public static bool IsUpper(this string word)
+        {
+            bool result = false;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i]==char.ToUpper(word[i]))
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Static.Example/Static.Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Static.Example.Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "lorem";
+            Console.WriteLine($"Result is {word.IsUpper()}");
+                        
+        }
+    }
+}
